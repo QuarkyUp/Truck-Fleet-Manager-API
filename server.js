e972324bfd55c0cf793defccf5e1068824216cea
QuarkyUp <thomasleflohic@gmail.com>
2017-12-20 15:25:45 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const app = require('express')();
+const API = require('json-api');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const APIError = API.types.Error;
+mongoose.connect(process.env.DB_URL, { useMongoClient: true });
+
+const models = {
+  City: require('./models/city').model,
+  Truck: require('./models/truck').model
+};
+
+const registryTemplates = {
+  cities: require('./models/city').registry,
+  trucks: require('./models/truck').registry
+};
+
+const adapter = new API.dbAdapters.Mongoose(models);
+const registry = new API.ResourceTypeRegistry(registryTemplates,
+  {dbAdapter: adapter});
+
+const docs = new API.controllers.Documentation(registry, {name: 'Truck API'});
+const controller = new API.controllers.API(registry);
+const front = new API.httpStrategies.Express(controller, docs);
+
+const apiReqHandler = front.apiRequest.bind(front);
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Cache-Control');
+  res.header('Access-Control-Allow-Methods',
+    'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+const db = [
+  'city',
+  'truck'
+];
+
+app.options('*', (req, res) => {
+  res.send();
+});
+
+app.get('/api', front.docsRequest.bind(front));
+
+app.get('/algo-gen/start/:cityNameStart/end/:cityNameEnd', (req, res) => {
+  res.send(`${JSON.stringify(req.params)}`);
+});
+
+app.route(`/api/:type(${db.join('|')})`).get(apiReqHandler).post(apiReqHandler)
+  .patch(apiReqHandler);
+
+app.route(`/api/:type(${db.join('|')})/:id`).get(apiReqHandler)
+  .patch(apiReqHandler)
+  .delete(apiReqHandler);
+
+app.route(`/api/:type(${db.join('|')})/:id/relationships/:relationship`)
+  .get(apiReqHandler).post(apiReqHandler).patch(apiReqHandler)
+  .delete(apiReqHandler);
+
+app.use((req, res) => {
+  front.sendError(new APIError(404, undefined, 'Not Found'), req, res);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on : ${process.env.BASE_URL}`);
+});
+
